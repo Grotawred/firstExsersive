f3be1ece771cb193a623f56e34c809c30b77348f
Grotawred <107578105+Grotawred@users.noreply.github.com>
2022-07-11 19:32:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-            String Line = new String("Ехал Гркека через реку видит Грека в реке рак сунул Грека руку в реку рак за руку Греку цап");
+        HashMap<Integer, String> myHashMap = new HashMap<>();
+            String Line = "Ехал Гркека через реку видит Грека в реке рак сунул Грека руку в реку рак за руку Греку цап";
             String[] findWodrs = new String[]{"Грека", "реку", "реке", "рак", "руку", "Греку"};
         ArrayList<String> Words = new ArrayList<>(Arrays.asList(Line.split(" ")));
+        String[] results = new String[findWodrs.length];
         for (String findWodr : findWodrs) {
             int count = 0;
             for (String word : Words) {
@@ -14,9 +15,12 @@ public class Main {
                     count++;
                 }
             }
-            System.out.println(findWodr + " - "+ count);
+            results[count] = results[count] + ", "+ findWodr;
         }
-
-
+        for(int i = 1; i< results.length;i++){
+            if(results[i] != null){
+                System.out.println(i + " - " + results[i]);
+            }
+        }
     }
 }
